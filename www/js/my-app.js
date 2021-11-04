4dd2892fd923985eaf5958f52e06e9a5e46a7f6a
Ezequiel Iadanza <echeiadanza7@hotmail.com>
2021-11-04 13:48:07 -0300

    error myapp

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,10 +15,13 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+
+      {path: '/index/',        url: 'index.html',},
+      
+      {path: '/pagina1/',      url: 'pagina1.html',},
+
+      {path: '/pagina2/',      url: 'pagina2.html',},
+      
     ]
     // ... other parameters
   });
@@ -37,8 +40,42 @@ $$(document).on('page:init', function (e) {
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
     alert('Hello');
+    
 })
+
+$$(document).on('page:init', '.page[data-name="pagina1"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+    
+  /*/export default (props, { $, $on }) => {
+    let $toolbarEl;
+
+    const togglePosition = () => {
+      $toolbarEl.toggleClass('toolbar-bottom toolbar-top');
+    }
+
+    $on('pageInit', (e, page) => {
+      $toolbarEl = page.$el.find('.toolbar');
+    });
+
+    return $render;
+  }
+ /*/ 
+})
+
+$$(document).on('page:init', '.page[data-name="pagina2"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+  alert('Hello');
+  
+})
+
+
+//funciones
+
+
+  
